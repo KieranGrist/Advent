ea9fbe1e347afd18139d04732922bfd02ba61ba6
Kieran Grist <kierangrist@gmail.com>
2021-12-02 09:03:29 +0000

    Day 2

--- Advent/Advent.cpp
+++ Advent/Advent.cpp
@@ -8,7 +8,7 @@
 #include <cstdint>
 #include <algorithm>
 #include <iomanip>
-using namespace::std;
+
 
 
 int main()
@@ -17,60 +17,54 @@ int main()
 	myfile.open("list.txt");
 	std::string mystring;
 	int count = 0;
-	int previous_sum_amount = -1;
-	vector<int> numbers;
+	int horizontal_position = 0;
+	int depth = 0;
+	int aim = 0;
+	std::string input;
+
 	if (myfile.is_open()) {
 		while (myfile.good())
 		{
 			myfile >> mystring;
-			int num = std::stoi(mystring);
-			numbers.push_back(num);
+			//	std::cout << mystring << "\n";
+
+			if (mystring == "forward" || mystring == "up" || mystring == "down")
+			{
+				input = mystring;
+				continue;
+			}
+
+			if (input == "forward")
+			{
+				int X = std::stoi(mystring);
+				horizontal_position += X;
+				
+				std::cout << "forward " << X << " horizontal position " << horizontal_position << "\n";
+				
+				if (aim != 0)
+				{
+					depth += aim * X;
+					std::cout  << "aim " << aim << " num " << X << " Depth " << depth << "\n";
+				}
+			}
+			if (input == "up")
+			{
+				int X = std::stoi(mystring);
+				//depth -= X;
+				aim -= X;
+				std::cout << "up " << X << " depth " << depth << " aim " << aim << "\n";			
+			}
+			if (input == "down")
+			{
+				int X = std::stoi(mystring);
+				//depth += X;
+				aim += X;
+				std::cout << "down " << X << " depth " << depth << " aim " << aim	 << "\n";
+			}
 		}
 	}
-
-	int alpha_index = 0;
-	int bravo_index = 1;
-	int charlie_index = 2;
-	
-	for (int i = 0; i < numbers.size(); i++)
-	{
-		alpha_index = i;
-		bravo_index = alpha_index + 1;
-		if (numbers.size() <= bravo_index)
-			break;
-		charlie_index = bravo_index + 1;
-		if (numbers.size() <= charlie_index)
-			break;
-
-		int alpha_num = numbers[alpha_index];
-		int bravo_num = numbers[bravo_index];
-		int charlie_num = numbers[charlie_index];
-
-		int current_sum = alpha_num + bravo_num + charlie_num;
-
-		if (previous_sum_amount == -1)
-		{
-			std::cout << current_sum << " (N/A - no previous measurement)" << "\n";
-			previous_sum_amount = current_sum;
-			continue;
-		}
-		if (current_sum > previous_sum_amount)
-		{
-
-			std::cout << current_sum << " (increased)" << "\n";
-			count++;
-		}
-		if (current_sum == previous_sum_amount)
-		{
-			std::cout << current_sum << " (no change)" << "\n";
-
-		}
-		if (current_sum < previous_sum_amount)
-		{
-			std::cout << current_sum << " (decreased)" << "\n";
-		}
-		previous_sum_amount = current_sum;
-	}
-
+	std::cout << "final depth " << depth << "\n";
+	std::cout << "final horizontal position " << horizontal_position << "\n";
+	count = horizontal_position * depth;
 	std::cout << "count " << count;
 }
