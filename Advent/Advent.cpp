f59d287a4a705a0bd0128c0c0c91bb640239c8ee
Kieran Grist <kierangrist@gmail.com>
2021-12-14 13:51:13 +0000

    day 3 extra fixed

--- Advent/Advent.cpp
+++ Advent/Advent.cpp
@@ -49,7 +49,21 @@ public:
 	{
 		Bits.push_back(InBit);
 	}
+
+	void RemoveRow(int InRow)
+	{
+		Bits.erase(Bits.begin() + InRow);
+	}
 	
+	void Print()
+	{
+		for (auto itr : Bits)
+		{
+			std::cout << itr;
+		}
+	}
+
+
 	const std::string& GetString()
 	{
 		return BinaryString;
@@ -59,6 +73,7 @@ private:
 	std::string BinaryString;
 	std::vector<int> Bits;
 };
+
 struct BinaryVector
 {
 public:
@@ -87,32 +102,219 @@ public:
 	{
 		if (FindBinaryStorage(InBinary.GetString()) != BinaryNumbers.end())
 			return;
+		
 		BinaryNumbers.emplace(InBinary.GetString(), InBinary);
 	}
 	
+	size_t Count()
+	{
+		return BinaryNumbers.size();
+	}
+
 	size_t RemoveBinaryNumber(std::string InString)
 	{
 		return BinaryNumbers.erase(InString);
 	}
 	
-	int Count()
+	std::map<std::string, BinaryNumber> BinaryNumbers;
+};
+
+
+struct OxgenVector : public BinaryVector
+{
+public: 
+	OxgenVector(const BinaryVector& InBinaryNumber)
 	{
-		return BinaryNumbers.size();
+		BinaryNumbers = InBinaryNumber.BinaryNumbers;
+	};
+
+	void Print(int Position)
+	{
+		std::cout << "Oxygen Vector Numbers Readout Position " << Position  << "\n";
+		for (auto itr = BinaryNumbers.begin(); itr != BinaryNumbers.end(); ++itr)
+		{
+			itr->second.Print();
+			std::cout << "\n";
+		}
 	}
 
-	std::map<std::string, BinaryNumber> BinaryNumbers;
+	std::string FindOxygenRating()
+	{
+		/*
+		 To find oxygen generator rating,
+		 determine the most common value (0 or 1) in the current bit position,
+		 and keep only numbers with that bit in that position. 
+		 if 0 and 1 are equally common, keep values with a 1 in the position being considered.
+		*/
+		size_t size = BinaryNumbers.size();
+		int pos = 0;
+		while (size != 1)
+		{
+			Print(pos);
+		
+			std::vector<std::string> zeros;
+			std::vector<std::string> ones;
+			for (auto itr = BinaryNumbers.begin(); itr != BinaryNumbers.end(); ++itr)
+			{
+				int row_num = itr->second.GetRow(0);
+				itr->second.RemoveRow(0);
+				switch (row_num)
+				{
+				case 0:
+					zeros.push_back(itr->first);
+					break;
+				case 1:
+					ones.push_back(itr->first);
+					break;
+				}
+			}
+
+			int zero_count = zeros.size();
+			int one_count = ones.size();
+			std::cout << "zero_count " << zero_count << " one_count " << one_count << " Count " << Count() << "\n";
+			if (zero_count > one_count)
+			{
+				for (auto itr : ones)
+				{
+					RemoveBinaryNumber(itr);
+					size = Count();
+					if (size == 1)
+						break;
+				}
+			}
+			
+			if (one_count > zero_count)
+			{
+				for (auto itr : zeros)
+				{
+					RemoveBinaryNumber(itr);
+					size = Count();
+					if (size == 1)
+						break;
+				}
+			}
+
+			if (one_count == zero_count)
+			{
+				
+				for (auto itr : zeros)
+				{
+					RemoveBinaryNumber(itr);
+					size = Count();
+					if (size == 1)
+						break;
+				}
+			}
+			pos++;
+		}
+
+		auto OxygenRating = BinaryNumbers.begin()->first;
+		std::cout << "OxygenRating " << OxygenRating << "\n";
+		return OxygenRating;
+	}
+};
+
+struct CO2Vector : public BinaryVector
+{
+public:
+	CO2Vector(const BinaryVector& InBinaryNumber)
+	{
+		BinaryNumbers = InBinaryNumber.BinaryNumbers;
+	};
+
+	void Print(int Position)
+	{
+		std::cout << "CO2 Vector Numbers Readout Position " << Position << "\n";
+		for (auto itr = BinaryNumbers.begin(); itr != BinaryNumbers.end(); ++itr)
+		{
+			itr->second.Print();
+			std::cout << "\n";
+		}
+	}
+
+	std::string FindC02ScruberRating()
+	{
+		/*
+		* To find CO2 scrubber rating, 
+		determine the least common value (0 or 1) in the current bit position,
+		and keep only numbers with that bit in that position.
+		If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+		*/
+
+		size_t size = BinaryNumbers.size();
+		int pos = 0;
+		while (size != 1)
+		{
+			Print(pos);
+			pos++;
+			std::vector<std::string> zeros;
+			std::vector<std::string> ones;
+			for (auto itr = BinaryNumbers.begin(); itr != BinaryNumbers.end(); ++itr)
+			{
+				int row_num = itr->second.GetRow(0);
+				itr->second.RemoveRow(0);
+				switch (row_num)
+				{
+				case 0:
+					zeros.push_back(itr->first);
+					break;
+				case 1:
+					ones.push_back(itr->first);
+					break;
+				}
+			}
+
+			int zero_count = zeros.size();
+			int one_count = ones.size();
+			std::cout << "zero_count " << zero_count << " one_count " << one_count << " Count " << Count() << "\n";
+
+			if (zero_count < one_count)
+			{
+				for (auto itr : ones)
+				{
+					RemoveBinaryNumber(itr);
+					size = Count();
+					if (size == 1)
+						break;
+				}
+			}
+
+			if (one_count < zero_count)
+			{
+				for (auto itr : zeros)
+				{
+					RemoveBinaryNumber(itr);
+					size = Count();
+					if (size == 1)
+						break;
+				}
+			}
+
+			if (one_count == zero_count)
+			{
+				for (auto itr : ones)
+				{
+					RemoveBinaryNumber(itr);
+					size = Count();
+					if (size == 1)
+						break;
+				}
+			}
+			size = Count();
+		}
+		auto C02ScruberRating = BinaryNumbers.begin()->first;
+		std::cout << "C02ScruberRating " << C02ScruberRating << "\n";
+		return C02ScruberRating;
+	}
 };
 int main()
 {
-	std::cout << "Hello, world!";
 	std::ifstream myfile;
 	std::string binary_string;
 	myfile.open("list.txt");
 	int count = 0;
 	int size = 0;
 	BinaryVector binary_vector;
-	std::map<int, BinaryVector> mapped_zeros;
-	std::map<int, BinaryVector> mapped_ones;
 	if (myfile.is_open())
 	{
 		while (myfile.good())
@@ -123,93 +325,17 @@ int main()
 			BinaryNumber number;
 			number.SetString(binary_string);
 			binary_vector.AddBinaryNumber(number);
-			for (int row = 0; row < size; row++)
-			{
-				//  std::cout << "binary string  " << num.GetString() << "\n";
-				int row_num = number.GetRow(row);
-				//  std::cout << "row " << row << " num " << row_num << "\n";
-				switch (row_num)
-				{
-				case 0:
-					mapped_zeros[row].AddBinaryNumber(number);
-					break;
-				case 1:
-					mapped_ones[row].AddBinaryNumber(number);
-					break;
-				}
-			}
 		}
 	}
 	// Find the Oxygen rating 
 	int numbers_left = 0;
-	BinaryVector oxygen_vector = binary_vector;
-	BinaryVector co2_vector = binary_vector;
-	//  std::cout << "oxygen generator start"<< "\n";
-	for (int position = 0; position < size; position++)
-	{
-		BinaryVector zeroes = mapped_zeros[position];
-		BinaryVector ones = mapped_ones[position];
-		int zero_count = zeroes.Count();
-		int one_count = ones.Count();
+	OxgenVector oxygen_vector = binary_vector;
+	CO2Vector co2_vector = binary_vector;
 
-		if (one_count > zero_count)
-		{
-			// oxygen_generator_number : Throw out all the numbers with a 0 in the position
-			for (auto itr = zeroes.BinaryNumbers.begin(); itr != zeroes.BinaryNumbers.end(); ++itr)
-			{
-				if (oxygen_vector.Count() == 1)
-					break;
-				oxygen_vector.RemoveBinaryNumber(itr->first);
-			}
-			// CO2_scrubber_rating : Throw out all numbers with a 1 in position 
-			for (auto itr = ones.BinaryNumbers.begin(); itr != ones.BinaryNumbers.end(); ++itr)
-			{
-				if (co2_vector.Count() == 1)
-					break;
-				co2_vector.RemoveBinaryNumber(itr->first);
-			}
-		}
-		if (zero_count > one_count)
-		{
-			// oxygen_generator_number : Throw out all the numbers with a 1 in the position
-			for (auto itr = ones.BinaryNumbers.begin(); itr != ones.BinaryNumbers.end(); ++itr)
-			{
-				if (oxygen_vector.Count() == 1)
-					break;
-				oxygen_vector.RemoveBinaryNumber(itr->first);
-			}
-			// CO2_scrubber_rating : Throw out all numbers with a 0 in position 
-			for (auto itr = zeroes.BinaryNumbers.begin(); itr != zeroes.BinaryNumbers.end(); ++itr)
-			{
-				if (co2_vector.Count() == 1)
-					break;
-				co2_vector.RemoveBinaryNumber(itr->first);
-			}
-		}
-		if (zero_count == one_count)
-		{
-			// oxygen_generator_number : Keep all the numbers with a 1 in the position 
-			for (auto itr = zeroes.BinaryNumbers.begin(); itr != zeroes.BinaryNumbers.end(); ++itr)
-			{
-				if (oxygen_vector.Count() == 1)
-					break;
-				oxygen_vector.RemoveBinaryNumber(itr->first);
-			}
-			// CO2_scrubber_rating : Throw out all numbers with a 0 in position 
-			for (auto itr = ones.BinaryNumbers.begin(); itr != ones.BinaryNumbers.end(); ++itr)
-			{
-				if (co2_vector.Count() == 1)
-					break;
-				co2_vector.RemoveBinaryNumber(itr->first);
-			}
-		}
-		if (co2_vector.Count() == 1 && oxygen_vector.Count() == 1)
-			break;
-	}
-	auto oxygen_generator_number = oxygen_vector.GetFirstBinaryNumber();
-	auto co2_binary_number = co2_vector.GetFirstBinaryNumber();
-	int oxygen_generator_rating = std::stoi(oxygen_generator_number.GetString(), nullptr, 2);
-	int CO2_scrubber_rating = std::stoi(co2_binary_number.GetString(), nullptr, 2);
+	auto oxygen_generator_number = oxygen_vector.FindOxygenRating();
+	auto co2_binary_number = co2_vector.FindC02ScruberRating();
+	int oxygen_generator_rating = std::stoi(oxygen_generator_number, nullptr, 2);
+	int CO2_scrubber_rating = std::stoi(co2_binary_number, nullptr, 2);
 	std::cout << "oxygen_generator_rating " << oxygen_generator_rating << " CO2_scrubber_rating " << CO2_scrubber_rating << "\n";
 	count = oxygen_generator_rating * CO2_scrubber_rating;
 	std::cout << "count " << count;
