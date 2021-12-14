35583e5413460027ceeebbf36d3285c8aa31f3e1
Kieran Grist <kierangrist@gmail.com>
2021-12-14 12:34:08 +0000

    Back up

--- Advent/Advent.cpp
+++ Advent/Advent.cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,96 +7,210 @@
 #include <cstdint>
 #include <algorithm>
 #include <iomanip>
+struct BinaryNumber
+{
+public:
+	BinaryNumber()
+	{
+		BinaryString.clear();
+		Bits.clear();
+	};
 
+	BinaryNumber(const BinaryNumber& InBinaryNumber)
+	{
+		Bits = InBinaryNumber.Bits;
+		BinaryString = InBinaryNumber.BinaryString;
+	};
+	
+	void SetString(std::string InString)
+	{
+		BinaryString = InString;
+		for (std::string::size_type i = 0; i < BinaryString.size(); i++)
+		{
+			int bin = -1;
+			if (BinaryString[i] == '0')
+			{
+				bin = 0;
+			}
+			else
+			{
+				bin = 1;
+			}
+			AddBit(bin);
+		}
+	}
+	
+	int GetRow(int InRow)
+	{
+		return Bits[InRow];
+	}
 
+	void AddBit(int InBit)
+	{
+		Bits.push_back(InBit);
+	}
+	
+	const std::string& GetString()
+	{
+		return BinaryString;
+	}
 
+private:
+	std::string BinaryString;
+	std::vector<int> Bits;
+};
+struct BinaryVector
+{
+public:
+	BinaryVector()
+	{
+		BinaryNumbers.clear();
+	}
+	
+	BinaryVector(const BinaryVector& InBinaryNumber)
+	{
+		BinaryNumbers = InBinaryNumber.BinaryNumbers;
+	};
+	
+	std::map<std::string, BinaryNumber>::iterator FindBinaryStorage(std::string InString)
+	{
+		return BinaryNumbers.find(InString);
+	}
+	
+	BinaryNumber GetFirstBinaryNumber()
+	{
+		auto number = BinaryNumbers.begin();
+		return number->second;
+	}
+	
+	void AddBinaryNumber(BinaryNumber InBinary)
+	{
+		if (FindBinaryStorage(InBinary.GetString()) != BinaryNumbers.end())
+			return;
+		BinaryNumbers.emplace(InBinary.GetString(), InBinary);
+	}
+	
+	size_t RemoveBinaryNumber(std::string InString)
+	{
+		return BinaryNumbers.erase(InString);
+	}
+	
+	int Count()
+	{
+		return BinaryNumbers.size();
+	}
+
+	std::map<std::string, BinaryNumber> BinaryNumbers;
+};
 int main()
 {
+	std::cout << "Hello, world!";
 	std::ifstream myfile;
+	std::string binary_string;
 	myfile.open("list.txt");
-	std::string mystring;
 	int count = 0;
-	
-	std::map<int, std::vector<int>> mapped_column_numbers;
-
-	if (myfile.is_open()) {
+	int size = 0;
+	BinaryVector binary_vector;
+	std::map<int, BinaryVector> mapped_zeros;
+	std::map<int, BinaryVector> mapped_ones;
+	if (myfile.is_open())
+	{
 		while (myfile.good())
 		{
-			myfile >> mystring;
-			std::cout << "mystring  " << mystring <<"\n";
-
-			for (std::string::size_type i = 0; i < mystring.size(); i++)
+			myfile >> binary_string;
+			//std::cout << "binary string " << binary_string << "\n";
+			size = binary_string.length();
+			BinaryNumber number;
+			number.SetString(binary_string);
+			binary_vector.AddBinaryNumber(number);
+			for (int row = 0; row < size; row++)
 			{
-				int bin = -1;
-				if (mystring[i] == '0')
+				//  std::cout << "binary string  " << num.GetString() << "\n";
+				int row_num = number.GetRow(row);
+				//  std::cout << "row " << row << " num " << row_num << "\n";
+				switch (row_num)
 				{
-					bin = 0;
-				}
-				else
-				{
-					bin = 1;
-				}
-			
-				std::cout << "column i " << i << " had binary " <<bin << "\n";
-				auto column_pair = mapped_column_numbers.find(i);
-				if (column_pair != mapped_column_numbers.end())
-				{
-					column_pair->second.push_back(bin);
-				}
-				else
-				{
-					std::vector<int> array;
-					array.push_back(bin);
-					mapped_column_numbers.emplace(i, array);
+				case 0:
+					mapped_zeros[row].AddBinaryNumber(number);
+					break;
+				case 1:
+					mapped_ones[row].AddBinaryNumber(number);
+					break;
 				}
 			}
 		}
 	}
-	std::string gamma = "";
-	std::string epsilon = "";
-
-	for (auto it = mapped_column_numbers.begin(); it != mapped_column_numbers.end(); it++)
+	// Find the Oxygen rating 
+	int numbers_left = 0;
+	BinaryVector oxygen_vector = binary_vector;
+	BinaryVector co2_vector = binary_vector;
+	//  std::cout << "oxygen generator start"<< "\n";
+	for (int position = 0; position < size; position++)
 	{
-		int zero = 0;
-		int one = 0;
-		std::vector<int> num_array = it->second;
-		for (int num : num_array)
+		BinaryVector zeroes = mapped_zeros[position];
+		BinaryVector ones = mapped_ones[position];
+		int zero_count = zeroes.Count();
+		int one_count = ones.Count();
+
+		if (one_count > zero_count)
 		{
-			switch (num)
+			// oxygen_generator_number : Throw out all the numbers with a 0 in the position
+			for (auto itr = zeroes.BinaryNumbers.begin(); itr != zeroes.BinaryNumbers.end(); ++itr)
 			{
-			case 0:
-				zero++;
-				break;
-			case 1:
-				one++;
-				break;			
+				if (oxygen_vector.Count() == 1)
+					break;
+				oxygen_vector.RemoveBinaryNumber(itr->first);
+			}
+			// CO2_scrubber_rating : Throw out all numbers with a 1 in position 
+			for (auto itr = ones.BinaryNumbers.begin(); itr != ones.BinaryNumbers.end(); ++itr)
+			{
+				if (co2_vector.Count() == 1)
+					break;
+				co2_vector.RemoveBinaryNumber(itr->first);
 			}
 		}
-
-		std::cout << "column  " << it->first << " zeros: " << zero << " ones: " << one << "\n";
-
-		if (zero > one)
+		if (zero_count > one_count)
 		{
-			gamma.append("0");
-			epsilon.append("1");
+			// oxygen_generator_number : Throw out all the numbers with a 1 in the position
+			for (auto itr = ones.BinaryNumbers.begin(); itr != ones.BinaryNumbers.end(); ++itr)
+			{
+				if (oxygen_vector.Count() == 1)
+					break;
+				oxygen_vector.RemoveBinaryNumber(itr->first);
+			}
+			// CO2_scrubber_rating : Throw out all numbers with a 0 in position 
+			for (auto itr = zeroes.BinaryNumbers.begin(); itr != zeroes.BinaryNumbers.end(); ++itr)
+			{
+				if (co2_vector.Count() == 1)
+					break;
+				co2_vector.RemoveBinaryNumber(itr->first);
+			}
 		}
-		else
+		if (zero_count == one_count)
 		{
-			gamma.append("1");
-			epsilon.append("0");
+			// oxygen_generator_number : Keep all the numbers with a 1 in the position 
+			for (auto itr = zeroes.BinaryNumbers.begin(); itr != zeroes.BinaryNumbers.end(); ++itr)
+			{
+				if (oxygen_vector.Count() == 1)
+					break;
+				oxygen_vector.RemoveBinaryNumber(itr->first);
+			}
+			// CO2_scrubber_rating : Throw out all numbers with a 0 in position 
+			for (auto itr = ones.BinaryNumbers.begin(); itr != ones.BinaryNumbers.end(); ++itr)
+			{
+				if (co2_vector.Count() == 1)
+					break;
+				co2_vector.RemoveBinaryNumber(itr->first);
+			}
 		}
-		std::cout << "gamma " << gamma << "\n";
-		std::cout << "epsilon " << epsilon << "\n";
-
-
+		if (co2_vector.Count() == 1 && oxygen_vector.Count() == 1)
+			break;
 	}
-	int gamma_rate = std::stoi(gamma, nullptr, 2);
-	int epsilon_rate = std::stoi(epsilon, nullptr, 2);
-
-	std::cout << "gamma_rate " << gamma_rate << "\n";
-	std::cout << "epsilon_rate " << epsilon_rate << "\n";
-
-	count = gamma_rate * epsilon_rate;
-
+	auto oxygen_generator_number = oxygen_vector.GetFirstBinaryNumber();
+	auto co2_binary_number = co2_vector.GetFirstBinaryNumber();
+	int oxygen_generator_rating = std::stoi(oxygen_generator_number.GetString(), nullptr, 2);
+	int CO2_scrubber_rating = std::stoi(co2_binary_number.GetString(), nullptr, 2);
+	std::cout << "oxygen_generator_rating " << oxygen_generator_rating << " CO2_scrubber_rating " << CO2_scrubber_rating << "\n";
+	count = oxygen_generator_rating * CO2_scrubber_rating;
 	std::cout << "count " << count;
 }
